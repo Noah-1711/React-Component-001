19777720329d4e1d99e6b5750194e50020bafd51
Noah-1711 <noahnirmal11@gmail.com>
2023-02-15 19:15:57 +0530

    resume

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+function Header(){
+    return(
+        <div className="headercontainer">
+            <h1>Noah Nirmal</h1>
+            <p> Full Stack Developer</p>
+            <div><a href="https://www.linkedin.com/in/noah-nirmal/">Linkedin</a> <a href="https://github.com/Noah-1711">Github</a></div>
+            <hr />
+        </div>
+    )
+}
+export default Header;
